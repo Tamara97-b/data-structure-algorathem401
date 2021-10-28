14b500f7b0e897fd3ad72089bb91acca1c1d0870
tamara97.b <tamarabassam1997@icloud.com>
2021-10-28 15:14:01 +0300

    done

--- stack-and-queue/lib/src/main/java/stack/and/queue/Animal/AnimalShelter.java
+++ stack-and-queue/lib/src/main/java/stack/and/queue/Animal/AnimalShelter.java
@@ -1,15 +1,20 @@
-package stack.and.queue.Animal;
-
-
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
-
-public class AnimalShelter {
-    Queue<Animal> animals = new Queue<Animal>();
-
-    public void enqueue(Animal animal){
-        animals.enqueue(animal);
-    }
-
-}
+//package stack.and.queue.Animal;
+//
+//
+//import java.sql.Array;
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.Iterator;
+//import java.util.Queue;
+//
+//public class AnimalShelter {
+//int position = 0 ;
+//ArrayList<String> list = new ArrayList<>();
+//int theLength = list.size();
+//while(list.size()=1){
+//
+//
+//
+//    }
+//
+//}
